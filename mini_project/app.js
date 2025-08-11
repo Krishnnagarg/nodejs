561b4c62979f465d783a9e2a35973730062b164b
Krishnnagarg <garg79408@gmail.com>
2025-08-11 13:25:17 +0530

    add mini_project

--- mini_project/app.js
+++ mini_project/app.js
@@ -1,2 +1,40 @@
-import readline form "readline";
+import readline from "readline";
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const todos = [];
+
+const showMenu = () => {
+  console.log("\n1: Add a Task");
+  console.log("2: View Task");
+  console.log("3: Exit");
+  rl.question("choose an option:", handleInput);
+};
+
+const handleInput = (option) => {
+  if (option === "1") {
+    rl.question("Enter Your Task :", (task) => {
+      todos.push(task);
+      console.log("Task Added :", task);
+      showMenu();
+    });
+  } else if (option === "2") {
+    console.log("\n Your Todo Lists");
+    todos.forEach((task,index) => {
+        console.log(`${index+1}. ${task}`)
+    })
+    showMenu();
+  } else if(option === "3") {
+    console.log(`Good Byee`);
+    rl.close();
+  } else {
+    console.log("Invalid option . Please Try Again");
+    showMenu();
+
+  }
+};
+
+showMenu();
